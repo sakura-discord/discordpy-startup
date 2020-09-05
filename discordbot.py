b8be15c81afc96afc4440ef5395d708bd377151a
AlphaNull <58626192+AlphaNull16299@users.noreply.github.com>
2020-09-05 19:59:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -83,8 +83,8 @@ async def dm(ctx, user : discord.User, *, message):
 #play
 @bot.event
 async def on_ready():
-    activity = discord.Game(name="kyonの靴下", type=2)
-    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    activity = discord.Game(name="kyonの靴下", type=3)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
     print("ぎばらは元気に動いてます")
               
 bot.run(token)
